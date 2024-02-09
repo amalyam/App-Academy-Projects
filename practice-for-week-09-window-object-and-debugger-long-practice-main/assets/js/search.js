01717613fc314463011df46a1f94e5d4db2f53bc
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-09 12:33:15 -0500

    implement functions

--- practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/search.js
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/search.js
@@ -1,29 +1,24 @@
-function findElementById(id) {
-    // Return the element in the DOM with corresponding `id`
-
-    // Your code here
+export function findElementById(id) {
+  // Return the element in the DOM with corresponding `id`
+  return document.getElementById(id);
 }
 
-function findFirstElementOfTag(tag) {
-    // Return the first occurence of an element of tag name `tag`
-
-    // Your code here
+export function findFirstElementOfTag(tag) {
+  // Return the first occurrence of an element of tag name `tag`
+  return findElementsOfTag(tag)[0];
 }
 
-function findFirstElementOfClass(cls) {
-    // Return the first occurence of an element of class `cls`
-
-    // Your code here
+export function findFirstElementOfClass(cls) {
+  // Return the first occurence of an element of class `cls`
+  return findElementsOfClass(cls)[0];
 }
 
-function findElementsOfTag(tag) {
-    // Return an array of elements that have a tag name of `tag`
-
-    // Your code here
+export function findElementsOfTag(tag) {
+  // Return an array of elements that have a tag name of `tag`
+  return [...document.getElementsByTagName(tag)];
 }
 
-function findElementsOfClass(cls) {
-    // Return an array of elements that have are of class `cls`
-
-    // Your code here
+export function findElementsOfClass(cls) {
+  // Return an array of elements that are of class `cls`
+  return [...document.getElementsByClassName(cls)];
 }
