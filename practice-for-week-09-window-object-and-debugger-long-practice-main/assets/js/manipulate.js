cc942ea918b6537f1ade053a9ebbf6f0c440cd76
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-09 12:25:46 -0500

    implement each function

--- practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/manipulate.js
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/manipulate.js
@@ -1,18 +1,15 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
-
-    // Your code here
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  document.title = "Amalya Megerman";
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
-
-    // Your code here
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  document.body.children[0].children[0].innerText = "I am Amalya M";
 }
 
-function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
-    // Your code here
+  document.body.children[1].children[1].innerText = "Hi hello it's me";
 }
