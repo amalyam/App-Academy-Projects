71ab663ea3797d5145501c5fc71d96f7968954f3
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-12 14:13:43 -0500

    complete phase 3

--- practice-for-week-10-express-static-files-main/server/app.js
+++ practice-for-week-10-express-static-files-main/server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use("/css", express.static("assets/css"));
+// app.use("/css", express.static("assets/css"));
 app.use("/scripts", express.static("assets/scripts"));
-app.use("/images", express.static("assets/images"));
+// app.use("/images", express.static("assets/images"));
+
+app.use("/stylesheets", express.static("assets/css"));
 
 const port = 5000;
 app.listen(port, () => console.log("Server is listening on port", port));
