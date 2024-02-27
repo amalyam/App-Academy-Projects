f756bd7027bd2b7b11acd3f611b43168e3c946d4
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-27 10:51:00 -0500

    complete

--- practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
+++ practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
@@ -1,19 +1,26 @@
 -- This ensures that SQLite enforces FOREIGN KEY constraints
 PRAGMA foreign_keys = 1;
+
 DROP TABLE IF EXISTS instruments;
+
 DROP TABLE IF EXISTS musicians;
+
 DROP TABLE IF EXISTS bands;
 
+DROP TABLE IF EXISTS musicians_instruments;
+
 CREATE TABLE bands (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(100)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100)
 );
+
 CREATE TABLE musicians (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(100) NOT NULL, last_name VARCHAR(100), band_id INTEGER NOT NULL, FOREIGN KEY (band_id) REFERENCES bands (id)
 );
+
 CREATE TABLE instruments (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  type VARCHAR(100) NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT, type VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE musicians_instruments (
+    musician_id INTEGER FOREIGN KEY NOT NULL, instrument_id INTEGER FOREIGN KEY NOT NULL
 );
