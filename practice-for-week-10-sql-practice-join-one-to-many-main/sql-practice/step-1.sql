cfe34677203a9918318485bc28f129eacef9a687
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-27 11:30:09 -0500

    complete

--- practice-for-week-10-sql-practice-join-one-to-many-main/sql-practice/step-1.sql
+++ practice-for-week-10-sql-practice-join-one-to-many-main/sql-practice/step-1.sql
@@ -1,3 +1,8 @@
 -- Step 1
+-- Run the SQL command that joins the bands and albums tables together, SELECTing both the name of the band and the album title.
 -- JOIN the tables, matching FOREIGN KEYs to the corresponding PRIMARY KEY.
 -- Your code here
+
+SELECT bands.name, albums.title
+FROM albums
+    JOIN bands ON (albums.band_id = bands.id);
--- practice-for-week-10-sql-practice-join-one-to-many-main/sql-practice/step-2.sql
+++ practice-for-week-10-sql-practice-join-one-to-many-main/sql-practice/step-2.sql
@@ -1,4 +1,11 @@
 -- Step 2
+-- Retrieve the names of the bands and the titles of their albums that were released in the year 2020.
 -- A WHERE clause can filter across any JOINed table, even if it's not in the
 -- final output of what is being SELECTed for.
 -- Your code here
+
+SELECT bands.name, albums.title
+FROM albums
+    JOIN bands ON albums.band_id = bands.id
+WHERE
+    albums.year = 2020;
