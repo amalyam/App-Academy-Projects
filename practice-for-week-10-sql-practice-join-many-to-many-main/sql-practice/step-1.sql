6db328a569f7a282756090c3c6ff5b631af6044f
Amalya Megerman <amalyamegerman@gmail.com>
2024-02-27 11:51:10 -0500

    complete

--- practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-1.sql
+++ practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-1.sql
@@ -2,4 +2,12 @@
 -- Connecting both sides of a many-to-many relationship involves JOINing the
 -- join table on to one side of the relationship, then JOINing the other side
 -- on to the join table.
--- Your code here
+-- Run the SQL command that joins the musicians and instruments tables together, SELECTing both the first name of the musician and the type of instrument.
+
+SELECT musicians.first_name, instruments.type
+FROM
+    musicians
+    JOIN musician_instruments ON (
+        musicians.id = musician_instruments.musician_id
+    )
+    JOIN instruments ON musician_instruments.instrument_id = instruments.id;
--- practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
+++ practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
@@ -1,4 +1,14 @@
 -- Step 2
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
--- Your code here
+-- Run the SQL command that selects the first and last name of each musician that plays the piano.
+
+SELECT musicians.first_name, musicians.last_name, instruments.type
+FROM
+    musicians
+    JOIN musician_instruments ON (
+        musicians.id = musician_instruments.musician_id
+    )
+    JOIN instruments ON musician_instruments.instrument_id = instruments.id
+WHERE
+    instruments.type = "piano";
