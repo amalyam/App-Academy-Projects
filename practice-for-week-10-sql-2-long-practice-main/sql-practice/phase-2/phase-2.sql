5bcfe12c28c38beae1f59886d9f6f531c5761b71
Amalya Megerman <amalyamegerman@gmail.com>
2024-03-01 10:16:18 -0500

    complete

--- practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-2/phase-2.sql
+++ practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-2/phase-2.sql
@@ -1,52 +1,72 @@
-PRAGMA foreign_keys=on; -- Turns foreign key support in SQLite3 on
--- Your code here
+PRAGMA foreign_keys = on;
+-- Turns foreign key support in SQLite3 on
 
-INSERT INTO owners
-  (first_name, last_name)
-VALUES
-  ('Nathan', 'Shanahan'),
-  ('Joycelyn', 'Cummerata'),
-  ('Weston', 'Jones'),
-  ('Melynda', 'Abshire'),
-  ('George', 'Beatty'),
-  ('Jonathan', 'Arbuckle'),
-  ('Hermione', 'Granger');
+DROP TABLE IF EXISTS owners;
 
-INSERT INTO cats
-  (name, birth_year)
-VALUES
-  ('Smudge', 2014),
-  ('Molly', 2015),
-  ('Lucky', 2016),
-  ('Bella', 2020),
-  ('Tiger', 2012),
-  ('Oscar', 2010),
-  ('Garfield', 2009),
-  ('Crookshanks', 2017);
+DROP TABLE IF EXISTS cats;
 
-INSERT INTO cat_owners
-  (cat_id, owner_id)
-VALUES
-  (1, 1),
-  (1, 2),
-  (2, 3),
-  (3, 3),
-  (4, 4),
-  (5, 4),
-  (5, 5),
-  (7, 6),
-  (8, 7);
+DROP TABLE IF EXISTS cat_owners;
 
-INSERT INTO toys
-  (cat_id, name)
-VALUES
-  (1, 'Catnip Mouse'),
-  (2, 'Feather Wand'),
-  (2, 'Scratcher'),
-  (2, 'Laser Pointer'),
-  (3, 'Chew Toy'),
-  (4, 'Tunnel'),
-  (4, 'Flopping Fish'),
-  (7, 'Cheetos'),
-  (8, 'Crinkle Ball'),
-  (8, 'Yarn');
+DROP TABLE IF EXISTS toys;
+
+CREATE TABLE owners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, last_name TEXT NOT NULL
+);
+
+CREATE TABLE cats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, name text NOT NULL, birth_year INTEGER NOT NULL
+);
+
+CREATE TABLE cat_owners (
+    owner_id INTEGER NOT NULL, cat_id INTEGER NOT NULL, FOREIGN KEY (owner_id) REFERENCES owners (id), FOREIGN KEY (cat_id) REFERENCES cats (id)
+);
+
+CREATE TABLE toys (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, cat_id INTEGER, FOREIGN KEY (cat_id) REFERENCES cats (id)
+);
+
+INSERT INTO
+    owners (first_name, last_name)
+VALUES ('Nathan', 'Shanahan'),
+    ('Joycelyn', 'Cummerata'),
+    ('Weston', 'Jones'),
+    ('Melynda', 'Abshire'),
+    ('George', 'Beatty'),
+    ('Jonathan', 'Arbuckle'),
+    ('Hermione', 'Granger');
+
+INSERT INTO
+    cats (name, birth_year)
+VALUES ('Smudge', 2014),
+    ('Molly', 2015),
+    ('Lucky', 2016),
+    ('Bella', 2020),
+    ('Tiger', 2012),
+    ('Oscar', 2010),
+    ('Garfield', 2009),
+    ('Crookshanks', 2017);
+
+INSERT INTO
+    cat_owners (cat_id, owner_id)
+VALUES (1, 1),
+    (1, 2),
+    (2, 3),
+    (3, 3),
+    (4, 4),
+    (5, 4),
+    (5, 5),
+    (7, 6),
+    (8, 7);
+
+INSERT INTO
+    toys (cat_id, name)
+VALUES (1, 'Catnip Mouse'),
+    (2, 'Feather Wand'),
+    (2, 'Scratcher'),
+    (2, 'Laser Pointer'),
+    (3, 'Chew Toy'),
+    (4, 'Tunnel'),
+    (4, 'Flopping Fish'),
+    (7, 'Cheetos'),
+    (8, 'Crinkle Ball'),
+    (8, 'Yarn');
--- practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-3/query-1.sql
+++ practice-for-week-10-sql-2-long-practice-main/sql-practice/phase-3/query-1.sql
@@ -1,2 +1,10 @@
 -- Find Hermione's cats
--- Your code here
+
+SELECT cats.name, owners.first_name
+FROM cats
+    JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+    JOIN owners ON (
+        owners.id = cat_owners.owner_id
+    )
+WHERE
+    owners.first_name = "Hermione";
