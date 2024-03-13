fd6c1ae4aaa163726979f6c0231fb8926fa3b841
Amalya Megerman <amalyamegerman@gmail.com>
2024-03-13 17:26:21 -0400

    complete

--- practice-for-week-11-sequelize-validations-and-constraints-short-practice-main/server/db/models/color.js
+++ practice-for-week-11-sequelize-validations-and-constraints-short-practice-main/server/db/models/color.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Color extends Model {
     /**
@@ -12,15 +10,30 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Color.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  }
+  Color.init(
+    {
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        validate: {
+          correctLength() {
+            if (this.name.length < 2 || this.name.length > 20) {
+              throw new Error("name must be between 2 and 20 characters");
+            }
+          },
+          checkEnding() {
+            if (this.name[this.name.length - 1] === "y") {
+              throw new Error("name must not end in 'y'");
+            }
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Color",
     }
-  }, {
-    sequelize,
-    modelName: 'Color',
-  });
+  );
   return Color;
 };
